b4720f388bd1af79583cd15112cf93ffc645d427
Umair Ashraf <umairjee360@gmail.com>
2025-12-07 21:43:28 +0500

    Layout-setup and routing started

---
+++ vision-code-frontend/src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+function NavBar() {
+  return (
+    <div>
+      <h4>Vision-Code</h4>
+    </div>
+  );
+}
+
+export default NavBar;
---
+++ vision-code-frontend/src/pages/AppLayout.jsx
@@ -0,0 +1,12 @@
+import NavBar from "../components/NavBar";
+
+function AppLayout() {
+  return (
+    <div>
+      <NavBar />
+      <h1>Welcome to Vision-Code</h1>
+    </div>
+  );
+}
+
+export default AppLayout;

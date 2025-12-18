12e7b83593a99357989af866d6d48d9aff52fca8
Hassan-rajpoot517 <bsef22m021@pucit.edu.pk>
2025-12-18 09:40:38 +0500

    roleFiedAddedInSignUpForm

--- vision_code_backend/Accounts/auth_serializers.py
+++ vision_code_backend/Accounts/auth_serializers.py
@@ -36,7 +36,7 @@ class SignupSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ("email", "password", "first_name", "last_name")
+        fields = ("email", "password", "first_name", "last_name","role")
 
     def create(self, validated_data):
         user = User.objects.create_user(
@@ -44,6 +44,7 @@ class SignupSerializer(serializers.ModelSerializer):
             password=validated_data["password"],
             first_name=validated_data.get("first_name", ""),
             last_name=validated_data.get("last_name", ""),
+            role=validated_data.get("role", "user"),
         )
         # create profile if not auto-created elsewhere
         UserProfile.objects.create(user=user)

7a96e3143a64e3a32e829a2ad48d9cc27d74543e
Umair Ashraf <140011143+umairCodesStack@users.noreply.github.com>
2026-05-08 15:31:37 +0500

    Update InstructorDashboard.jsx

--- src/pages/InstructorDashboard.jsx
+++ src/pages/InstructorDashboard.jsx
@@ -389,7 +389,7 @@ export default function InstructorDashboard() {
 
           <div className="p-6">
             {courses.length === 0 ? (
-              
+              <></>
             ) : (
               <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
                 {courses.map((course) => (
